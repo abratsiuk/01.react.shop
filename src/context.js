887e252b10a6c03d01f34a70b31ed4a67b2dbe19
Aleksandr Bratsiuk <bratsiuk@outlook.com>
2024-11-04 17:19:31 +0200

    add reducer

--- src/context.js
+++ src/context.js
@@ -1,10 +1,27 @@
-import React, { createContext } from 'react';
+import React, { createContext, useReducer } from 'react';
+import { reducer } from './reducer';
 
 export const ShopContext = createContext();
 
+const initialState = {
+    goods: [],
+    loading: true,
+    order: [],
+    isBasketShow: false,
+    alertName: '',
+};
+
 export const ContextProvider = ({ children }) => {
-    const value = {
-        example: 'example context',
+    const [value, dispatch] = useReducer(reducer, initialState);
+
+    value.addToBasket = (item) => {
+        dispatch({ type: 'ADD_TO_BASKET', payload: { item } });
+    };
+    value.removeFromBasket = (id) => {
+        dispatch({ type: 'REMOVE_FROM_BASKET', payload: { id } });
+    };
+    value.clearAlert = () => {
+        dispatch({ type: 'CLEAR_ALERT' });
     };
 
     return (
